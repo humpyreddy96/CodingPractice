a2b69c4230277eb9071e74add1582eac4423720e
humpyreddy96 <hp411819@ohio.edu>
2021-06-10 16:24:46 -0400

    final commit

--- 4_tournament_winner.py
+++ 4_tournament_winner.py
@@ -35,3 +35,25 @@ for i in range(len(competitions)):
 max_key = max(scores, key=scores.get)
 print(max_key)
 
+
+#Using Functions like a pro
+def tournamentWinner(competitions, results):
+    # Write your code here.
+	currentBestTeam = ""
+	scores = {currentBestTeam:0}
+	HOME_TEAM_WON = 1
+	
+	for idx,competition in enumerate(competitions):
+		result = results[idx]
+		homeTeam,awayTeam = competition
+		winningTeam = homeTeam if result==HOME_TEAM_WON else awayTeam
+		updateScores(winningTeam,3,scores)
+		if scores[currentBestTeam] < scores[winningTeam]:
+			currentBestTeam = winningTeam
+	return currentBestTeam
+
+def updateScores(team,points,scores):
+	if team not in scores:
+		scores[team]=0
+	scores[team] +=points
+
